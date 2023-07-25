90f7c4fddf70ed6922461d72e7e745fdd52cfe23
Ratnesh Patil <ratneshdesigns@gmail.com>
2023-07-25 23:33:56 +0530

    update homepage banner and trips cards

---
+++ src/components/layout/Banner/TypewriterAnimation.js
@@ -0,0 +1,26 @@
+import { TypeAnimation } from 'react-type-animation';
+
+const TypewriterAnimation = () => {
+
+    return (
+        <TypeAnimation
+            sequence={[
+                // Same substring at the start will only be typed out once, initially
+                'Tours.',
+                1000,
+                'Treks',
+                1000,
+                'Adventures',
+                1000,
+                'Experiences',
+                1000
+            ]}
+            wrapper="span"
+            speed={50}
+            style={{ fontSize: '3em', display: 'inline-block' }}
+            repeat={Infinity}
+        />
+    );
+};
+
+export default TypewriterAnimation;
