c12167d6fc8f9d5ab51d69337f09e8788d063b89
Ratnesh Patil <ratneshdesigns@gmail.com>
2023-07-29 20:11:25 +0530

    feat: BAC-15 - Update footer

--- src/components/ui/Slider/index.js
+++ src/components/ui/Slider/index.js
@@ -10,7 +10,7 @@ import {
 const Slider = (args) => {
 
     return (
-        <div className="container py-5 px-0">
+        <div className="container py-5 mb-5">
             <h2 className="mb-4">We <i className="bi bi-suit-heart-fill text-secondary" color="secondary"></i> to make memories with <span className="text-secondary">you!</span></h2>
             {/* TODO - Make this slider horizontal, and add a shading gradient over */}
             <div className="slider-container" md="12">
