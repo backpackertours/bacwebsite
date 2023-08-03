fef69a4aede97dd7f949377bf7ec308dff951b8a
Ratnesh Patil <ratneshdesigns@gmail.com>
2023-08-03 08:58:34 +0530

    feat: BAC-17 - Update trip details page ui

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-// import Navigation from './components/layout/Navigation';
-import Banner from './components/layout/Banner';
+import Navigation from './components/layout/Navigation';
+// import Banner from './components/layout/Banner';
 // import UpTrips from './components/ui/UpTrips';
 // import WeekendTrips from './components/ui/WeekendTrips';
 // import Testimonials from './components/ui/Testimonials';
-// import Footer from './components/layout/Footer';
-// import TripDetails from './components/ui/TripDetails';
+import Footer from './components/layout/Footer';
+import TripDetails from './components/ui/TripDetails';
 // import Slider from './components/ui/Slider';
 
 function App() {
   return (
     <div className="App">
-      {/* <Navigation expand="md" horizontal="end" /> */}
+      <Navigation expand="md" horizontal="end" />
       {/* Home Page */}
-
-      <Banner />
       {/* 
+      <Banner />
       <UpTrips />
       <WeekendTrips />
       <Testimonials />
@@ -22,8 +21,8 @@ function App() {
       */}
 
       {/* Trip Details Page */}
-      {/* <TripDetails /> */}
-      {/* <Footer /> */}
+      <TripDetails />
+      <Footer />
     </div>
   );
 }
