f730b9073b8f88bf67baa8c9496864c735aa75eb
Ratnesh Patil <ratneshdesigns@gmail.com>
2023-08-27 00:36:46 +0530

    feat: BAC-32 - add more options to navbar

--- src/components/layout/Navigation/index.js
+++ src/components/layout/Navigation/index.js
@@ -2,12 +2,17 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import {
+    Button,
     Collapse,
     Navbar,
     NavbarToggler,
     NavbarBrand,
     Nav,
-    NavItem
+    NavItem,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem
 } from 'reactstrap';
 
 import logo from '../../../assets/images/logo.png';
@@ -26,27 +31,57 @@ const Navigation = (args) => {
                 <NavbarToggler onClick={toggle} />
                 <Collapse isOpen={isOpen} navbar>
                     <Nav className="ms-auto" navbar>
-                        <NavItem className="me-5">
+                        <NavItem className="me-4">
                             <Link className="nav-link" to="/">Home</Link>
                         </NavItem>
-                        <NavItem className="me-5">
+                        <UncontrolledDropdown nav inNavbar className="me-4">
+                            <DropdownToggle nav caret>
+                                Upcoming Trips
+                            </DropdownToggle>
+                            <DropdownMenu center className="shadow-md">
+                                <DropdownItem>
+                                    <Link className="nav-link py-0" to="/trip">Spiti Valley Trek</Link>
+                                </DropdownItem>
+                                <DropdownItem>
+                                    <Link className="nav-link py-0" to="/trip">Himachal Pradesh</Link>
+                                </DropdownItem>
+                                <DropdownItem>
+                                    <Link className="nav-link py-0" to="/trip">Leh Ladakh</Link>
+                                </DropdownItem>
+                                <DropdownItem>
+                                    <Link className="nav-link py-0" to="/trip">Meghalaya Paradise</Link>
+                                </DropdownItem>
+                            </DropdownMenu>
+                        </UncontrolledDropdown>
+                        <UncontrolledDropdown nav inNavbar className="me-4">
+                            <DropdownToggle nav caret>
+                                Weekend Gateways
+                            </DropdownToggle>
+                            <DropdownMenu center className="shadow-md">
+                                <DropdownItem>
+                                    <Link className="nav-link py-0" to="/trip">Matheran Trek</Link>
+                                </DropdownItem>
+                                <DropdownItem>
+                                    <Link className="nav-link py-0" to="/trip">Garbette Plateu</Link>
+                                </DropdownItem>
+                                <DropdownItem>
+                                    <Link className="nav-link py-0" to="/trip">Mumbai Backwaters</Link>
+                                </DropdownItem>
+                                <DropdownItem>
+                                    <Link className="nav-link py-0" to="/trip">Kalavantin Durg</Link>
+                                </DropdownItem>
+                                {/* <DropdownItem divider />
+                                <DropdownItem>Reset</DropdownItem> */}
+                            </DropdownMenu>
+                        </UncontrolledDropdown>
+                        <NavItem className="me-4">
                             <Link className="nav-link" to="/aboutus">About Us</Link>
                         </NavItem>
                         <NavItem>
-                            <Link className="nav-link" to="/contactus">Contact Us</Link>
+                            <Button className="text-white rounded-pill" color="primary">
+                                <Link className="nav-link py-0 text-white" to="/contactus">Contact Us</Link>
+                            </Button>
                         </NavItem>
-                        {/* TODO - Remove dropdown if not needed */}
-                        {/* <UncontrolledDropdown nav inNavbar>
-                            <DropdownToggle nav caret>
-                                Options
-                            </DropdownToggle>
-                            <DropdownMenu right>
-                                <DropdownItem>Option 1</DropdownItem>
-                                <DropdownItem>Option 2</DropdownItem>
-                                <DropdownItem divider />
-                                <DropdownItem>Reset</DropdownItem>
-                            </DropdownMenu>
-                        </UncontrolledDropdown> */}
                     </Nav>
                 </Collapse>
             </Navbar>
