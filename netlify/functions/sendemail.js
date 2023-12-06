5e89faef4fa642bd6bedabccd7a00f60f2511229
Ratnesh Patil <ratneshdesigns@gmail.com>
2023-12-06 08:58:51 +0530

    saperate contact us and trip enquiry email templates

--- netlify/functions/sendemail.js
+++ netlify/functions/sendemail.js
@@ -17,14 +17,14 @@ exports.handler = async function (event, context) {
             }
         });
 
-        // generate HTML body
-        const renderedHtml = `
+        // render this html for response email to trip inquiry
+        const renderedHtmlForTripEnquiry = `
             <!DOCTYPE html>
                 <html lang="en">
                 <head>
                     <meta charset="UTF-8">
                     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                    <title>${trip ? `Inquiry for ${trip}` : `Thanks for the Inquiry, ${name}!`}</title >
+                    <title>Inquiry for ${trip}</title >
                     <style>
                         body {
                             font - family: 'Arial', sans-serif;
@@ -70,7 +70,7 @@ exports.handler = async function (event, context) {
                 <body>
                     <div class="container">
                         <div class="title">
-                            <h2>${trip ? `Inquiry for ${trip}` : `Thanks for the Inquiry, ${name}!`}</h2>
+                            <h2>Inquiry for ${trip}</h2>
                         </div>
                         <div class="body-content">
                             <p>Thank you for reaching out to us! We have received your inquiry and would like to express our
@@ -80,8 +80,86 @@ exports.handler = async function (event, context) {
                                 or +91 8082647928, +91 8286835202.
                             </p>
                             <h3>Details You Shared</h3>
-                            ${trip && `<p>${trip}</p>`}
-                            ${dates && `<p>${dates}</p>`}
+                            <p>${trip}</p>
+                            <p>${dates}</p>
+                            <p>Name: ${name}</p>
+                            <p>Email: ${email}</p>
+                            <p>Contact No: ${phone}</p>
+                            <p>Message: ${message}</p>
+                        </div>
+                        <div class="footer">
+                            <img class="logo"
+                                src="https://res.cloudinary.com/dgjllfp17/image/upload/v1699731972/backpackers/logo_ifq8uf.png">
+                                <p>Email - backpackmumbai@gmail.com</p>
+                                <p>Phone - +91 8082647928, +91 8286835202</p>
+                        </div>
+                    </div>
+                </body>
+            </html >`
+
+        // render this html for response email for contact us enquiry
+        const renderedHtmlForContactUs = `
+            <!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Thanks for the Inquiry, ${name}</title >
+                    <style>
+                        body {
+                            font - family: 'Arial', sans-serif;
+                        margin: 0;
+                        padding: 0;
+                        background-color: #f4f4f4;
+                                    }
+
+                        .container {
+                            max - width: 600px;
+                        margin: 0 auto;
+                        padding: 20px;
+                        background-color: #ffffff;
+                                    }
+
+                        .title {
+                            text - align: center;
+                        color: #333;
+                        font-size: 24px;
+                        margin-bottom: 20px;
+                                    }
+
+                        .body-content {
+                            color: #555;
+                        font-size: 16px;
+                        line-height: 1.5;
+                        margin-bottom: 30px;
+                        text-align: center;
+                                    }
+
+                        .footer {
+                            text - align: center;
+                        color: #777;
+                        font-size: 16px;
+                                    }
+
+                        .logo {
+                            width: 40%;
+                        height: auto;
+                                    }
+                    </style>
+                </head >
+                <body>
+                    <div class="container">
+                        <div class="title">
+                            <h2>Thanks for the Inquiry, ${name}!</h2>
+                        </div>
+                        <div class="body-content">
+                            <p>Thank you for reaching out to us! We have received your inquiry and would like to express our
+                                appreciation for considering Backpacker Tours.
+                                We will get back to you as soon as possible with the information you need. If you have any additional
+                                details to share or questions in the meantime, feel free to reach out to us at backpackmumbai@gmail.com
+                                or +91 8082647928, +91 8286835202.
+                            </p>
+                            <h3>Details You Shared</h3>
                             <p>Name: ${name}</p>
                             <p>Email: ${email}</p>
                             <p>Contact No: ${phone}</p>
@@ -102,7 +180,7 @@ exports.handler = async function (event, context) {
             from: process.env.SENDER,
             to: `${email}, ${process.env.SENDER} `,
             subject: `Thanks for Inquiry ${name}!`,
-            html: renderedHtml,
+            html: trip ? renderedHtmlForTripEnquiry : renderedHtmlForContactUs,
         });
 
         return {
