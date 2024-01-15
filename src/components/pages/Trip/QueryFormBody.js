320ed56b3fbebed977147dabedbafa235ee5d64a
Ratnesh Patil <ratneshdesigns@gmail.com>
2024-01-15 22:20:57 +0530

    update messagePattern

--- src/components/pages/Trip/QueryFormBody.js
+++ src/components/pages/Trip/QueryFormBody.js
@@ -79,7 +79,7 @@ const QueryFormBody = ({ trip }) => {
 
 
         //validate message
-        const messagePattern = /^[A-Za-z0-9\s]+$/
+        const messagePattern = /^[a-zA-Z0-9.!_@ ]*$/
         const messageMatch = messagePattern.test(form.message.value)
 
         // if any of the input is not matching set up the validation errors.
